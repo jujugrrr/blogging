097860e08239c614f4658506457674ecf0985b3f
jujugrrr <jujugrrr@gmail.com>
2014-05-16 22:54:21 +0100

    Initial commit, empty cookbook

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# 0.1.0
+
+Initial release of blogging
+
+* Enhancements
+  * an enhancement
+
+* Bug Fixes
+  * a bug fix
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'blogging'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures blogging'
+long_description 'Installs/Configures blogging'
+version          '0.1.0'
+
