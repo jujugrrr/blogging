29c90d219ad1c5f1626948762dc21999dbffb2dc
jujugrrr <jujugrrr@gmail.com>
2014-05-17 12:24:18 +0100

    Added helper for HTTP request to serverspec

--- test/integration/default/serverspec/spec_helper.rb
+++ test/integration/default/serverspec/spec_helper.rb
@@ -1,5 +1,7 @@
 require 'serverspec'
 require 'pathname'
+require 'net/http'
+
 
 include Serverspec::Helper::Exec
 include Serverspec::Helper::DetectOS
@@ -8,4 +10,11 @@ RSpec.configure do |c|
   c.before :all do
     c.os = backend(Serverspec::Commands::Base).check_os
   end
+end
+
+def http_request(uri, ip = '127.0.0.1')
+  uri = URI.parse(uri)
+  http = Net::HTTP.new(ip, uri.port)
+  request = Net::HTTP::Get.new(uri.request_uri, {'Host' => uri.host})
+  http.request(request)
 end
