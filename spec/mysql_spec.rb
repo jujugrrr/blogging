bdfc13173555ca4b0d111ce6482dae4c6fdcca30
jujugrrr <jujugrrr@gmail.com>
2014-05-18 10:52:54 +0100

    Add more test to spec/mysql_spec.rb (chef-solo restriction and mysql::client)

--- spec/mysql_spec.rb
+++ spec/mysql_spec.rb
@@ -1,10 +1,25 @@
 require 'spec_helper'
 
 describe 'blogging::mysql' do
-    let(:chef_run) { ChefSpec::Runner.new(platform: 'ubuntu', version: '12.04').converge(described_recipe) }
+    let(:chef_run) {
+      ChefSpec::Runner.new(platform: 'ubuntu', version: '12.04') do |node|
+        node.set[:rackspace_mysql][:server_debian_password] = 'TestDebianPassword'
+        node.set[:rackspace_mysql][:server_root_password] = 'TestRootPassword'
+        node.set[:rackspace_mysql][:server_repl_password] = 'TestReplicationPassword'
+      end.converge(described_recipe)
+    }
 
-    it 'include rackspace_mysql recipe' do
+    it 'include rackspace_mysql::server recipe' do
       expect(chef_run).to include_recipe('rackspace_mysql::server')
+    end
+    it 'include rackspace_mysql::client recipe' do
+      expect(chef_run).to include_recipe('rackspace_mysql::client')
+    end
+
+    it 'installs mysqlclient libs' do
+      expect(chef_run).to install_package('mysql-client')
+      expect(chef_run).to install_package('libmysqlclient-dev')
+      expect(chef_run).to install_package('ruby-mysql')
     end
 
 end
