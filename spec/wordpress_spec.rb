954b3b1ba64685cc8376fa4e55e244706d730484
jujugrrr <jujugrrr@gmail.com>
2014-05-18 11:41:34 +0100

    Added base chefspec for wordpress

---
+++ spec/wordpress_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe 'blogging::wordpress' do
+    let(:chef_run) {
+      ChefSpec::Runner.new(platform: 'ubuntu', version: '12.04') do |node|
+        node.set[:rackspace_sysql][:server_debian_password] = 'TestDebianPassword'
+        node.set[:rackspace_mysql][:server_root_password] = 'TestRootPassword'
+        node.set[:rackspace_mysql][:server_repl_password] = 'TestReplicationPassword'
+        node.set[:rackspace_mysql][:server_repl_password] = 'TestReplicationPassword'
+      end.converge(described_recipe)
+    }
+
+    it 'include wordpress::default recipe' do
+      expect(chef_run).to include_recipe('wordpress::default')
+    end
+
+    it 'creates a config file for wordpress' do
+      expect(chef_run).to render_file('/opt/www/wp-config.php').with_content('WordPress')
+    end
+
+end
