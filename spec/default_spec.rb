5d095ed4fb1fda4fd519eee35f908bb538c9e564
jujugrrr <jujugrrr@gmail.com>
2014-05-18 13:44:45 +0100

    Added stubs and attributes as it is required by other recipes

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -1,7 +1,21 @@
 require 'spec_helper'
 
 describe 'blogging::default' do
-    let(:chef_run) { ChefSpec::Runner.new(platform: 'ubuntu', version: '12.04').converge(described_recipe) }
+    let(:chef_run) {
+      ChefSpec::Runner.new(platform: 'ubuntu', version: '12.04') do |node|
+        node.set[:rackspace_mysql][:server_debian_password] = 'TestDebianPassword'
+        node.set[:rackspace_mysql][:server_root_password] = 'TestRootPassword'
+        node.set[:rackspace_mysql][:server_repl_password] = 'TestReplicationPassword'
+      end.converge(described_recipe)
+    }
+
+    before do
+      stub_command("egrep 'mirror.rackspace.com/ubuntu/? precise' /etc/apt/sources.list").and_return(false)
+      stub_command("egrep 'mirror.rackspace.com/ubuntu/? precise-updates' /etc/apt/sources.list").and_return(false)
+      stub_command("egrep 'mirror.rackspace.com/ubuntu/? precise-backports' /etc/apt/sources.list").and_return(false)
+      stub_command("egrep 'mirror.rackspace.com/ubuntu/? precise-security' /etc/apt/sources.list").and_return(false)
+      stub_command('test -f /var/lib/apt/periodic/update-success-stamp').and_return(true)
+    end
 
     it 'include blogging::nginx recipe' do
       expect(chef_run).to include_recipe('blogging::nginx')
