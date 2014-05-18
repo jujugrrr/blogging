936aa8ccf4b2200abf0f0f385726a77327a554b7
jujugrrr <jujugrrr@gmail.com>
2014-05-18 13:27:18 +0100

    Updated dependencies versions

--- metadata.rb
+++ metadata.rb
@@ -9,6 +9,6 @@ version          '0.1.0'
 
 depends 'rackspace_nginx', '>= 3.2.0'
 depends 'rackspace_php', '>= 2.3.1'
-depends 'pure-ftpd' , "0.1.0"
-depends 'rackspace_mysql'
-depends 'wordpress'
+depends 'pure-ftpd' , '0.1.0'
+depends 'rackspace_mysql', '>= 6.1.0'
+depends 'wordpress', '>= 2.1.4'
